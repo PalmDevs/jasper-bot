e1d21920ddaa4174c88c81f83e49fbbdbe876bd4
PalmDevs <git@palmdevs.me>
2025-07-06 20:18:48 +0700

    feat(commands/fun/is): make responses less random
    :)

--- src/commands/fun/is.ts
+++ src/commands/fun/is.ts
@@ -1,8 +1,28 @@
-import { ApplicationCommandOptionTypes } from 'oceanic.js'
+import { distance } from 'fastest-levenshtein'
+import { ApplicationCommandOptionTypes, type Message } from 'oceanic.js'
 import { setTimeout as setTimeoutPromise } from 'timers/promises'
 import { ChatCommand } from '~/classes/commands/ChatCommand'
 import { CommandTriggers, DefaultCommandContexts, DefaultCommandIntegrationTypes } from '~/classes/commands/Command'
+import { SortedLRUMap } from '~/classes/SortedLRUMap'
 import { s, string } from '~/strings'
+import { SimHashNgramSize, simHash } from '~/utils/hashes'
+
+// Message -> Response Index
+const recentResponses = new WeakMap<Message, number>()
+// Hash -> Response Index
+const recentQuestions = new SortedLRUMap<number>(100)
+const MaxDiffPercent = 0.15 // 15% difference
+
+const MaxConfirmationResponseDistance = 3
+const ConfirmationResponses = [
+    'seriously',
+    'you sure',
+    'are you sure',
+    'is that ',
+    'are you serious',
+    'are you kidding',
+    'is that really',
+]
 
 export default new ChatCommand({
     name: 'is',
@@ -32,6 +52,7 @@ export default new ChatCommand({
             name: 'question',
             description: 'What is it?',
             type: ApplicationCommandOptionTypes.STRING,
+            greedy: true,
             required: false,
         },
     ],
@@ -41,14 +62,60 @@ export default new ChatCommand({
     async execute(context, { question }, actions) {
         if (!question) return
 
+        // Remove any common special characters from the question
+        question = question
+            .toLowerCase()
+            .replaceAll(/[.?!,*#]+$/g, '')
+            .trim()
+
         const { trigger } = context
-        const { channel } = trigger
 
-        if (channel) await channel.client.rest.channels.sendTyping(channel.id)
-        await setTimeoutPromise(500)
+        if (trigger.messageReference && trigger.channel)
+            // The user is asking us to confirm a previous response
+            for (const response of ConfirmationResponses)
+                if (distance(question, response) <= MaxConfirmationResponseDistance) {
+                    // Attempt to get message from cache
+                    const msg = trigger.channel.messages.get(trigger.messageReference.messageID!)
+                    if (!msg) return
 
-        await actions.reply({
-            content: string(s.command.is.response),
+                    const responseIndex = recentResponses.get(msg)
+                    if (responseIndex !== undefined)
+                        return await actions.reply({
+                            content: string(s.command.is.response[responseIndex]!),
+                        })
+                }
+
+        // Always pad the question so it's always more than SimHashNgramSize characters long
+        // And to ensure the hash is similar for a similar question without padding
+        question += ' '.repeat(SimHashNgramSize)
+
+        const { channelID, client } = trigger
+
+        await client.rest.channels.sendTyping(channelID)
+        await setTimeoutPromise(1000)
+
+        const arr = simHash.compute_bitarray(question)
+        let hash = 0
+        for (let i = 0; i < 48; i++) hash |= arr[i]! << i
+
+        const { response } = s.command.is
+
+        const closest = recentQuestions.getClosest(hash)
+        if (closest && Math.abs(closest.key - hash) <= hash * MaxDiffPercent) {
+            recentQuestions.set(hash, closest.value)
+            // If the closest question is within 10% of the hash, return the same response
+            return await actions.reply({
+                content: string(response[closest.value]!),
+            })
+        }
+
+        const idx = Math.floor(Math.random() * response.length)
+        recentQuestions.set(hash, idx)
+
+        const msg = await actions.reply({
+            content: string(s.command.is.response[idx]!),
         })
+
+        recentResponses.set(msg, idx)
     },
 })
